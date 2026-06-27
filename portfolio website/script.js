1652b73f021dec1bd73a74fecc4ba22527582dbb
kelsey <kpdiep08@gmail.com>
2026-06-26 21:17:23 -0400

    Creating project
    I forgot to create a GitHub repo for my website... I'm very thankful that I didn't break anything! Finished the general functionality of the Home, Photoshop, and ibisPaint X pages, and finished the general layout for the Home and Photoshop pages. Also uploaded files for most of my artworks.

---
+++ portfolio website/script.js
@@ -0,0 +1,21 @@
+/**make an array of image names for the book
+ * on button click, trigger function
+ * 
+ * idea (written in java)
+ * int count = 0 (declare outside of the function)
+ * check iteration:
+ * if (count == list.length) {
+ *      count = 0
+ * }
+ * for (count; count < list.length; count++){
+ *     
+ * }
+ * 
+ * the querySelectorAll() function returns all elements that match the css selector as a nodelist
+ * 
+ */
+
+// var pageTitle = document.querySelectorAll('h2');
+// for (i = 0; i < pageTitle.length; i++) {
+//     pageTitle[i].style.
+// }
